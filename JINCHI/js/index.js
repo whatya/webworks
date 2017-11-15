62676051f8ffdfd7e869309d7ee5a04f1a992200
张宝 <2398772538@qq.com>
2017-11-15 21:24:28 +0800

    1、完成出新闻以外的功能；

--- JINCHI/js/index.js
+++ JINCHI/js/index.js
@@ -1,6 +1,8 @@
 
 var INDEX_URL       = "http://120.78.206.170/api/homepage/all"
 
+var INDEX_BANNERS   = "http://120.78.206.170/api/banner/list"
+
 var ABOUT_INTRO     = "http://120.78.206.170/api/about/list?parent=0"
 var ABOUT_MAN       = "http://120.78.206.170/api/about/list?parent=1"
 var ABOUT_HONOR     = "http://120.78.206.170/api/about/list"
@@ -10,16 +12,18 @@ var CULTURE_HISTORY = "http://120.78.206.170/api/culture/list"
 var CULTURE_MISSION = "http://120.78.206.170/api/culture/list?parent=1"
 var CULTURE_LIFE    = "http://120.78.206.170/api/culture/list?parent=2"
 
+var COMPANY_NEWS    = "http://120.78.206.170/api/news/list"
+var INDUSTRY_NEWS   = "http://120.78.206.170/api/news/list?parent=1"
+
+var PRODUCTS_LIST   = "http://120.78.206.170/api/product/list"
+var PRODUCT_DETAIL  = "http://120.78.206.170/api/product/getById?id="
+
 // **************************************页面加载运行 start*************************************
 
-//导航栏添加点击事件
-makeNavClickable();
 
-//二级子菜单添加点击事件
-makeMenuClickable();
 
-//首页获取并处理数据
-indexInit();
+//全局数据处理
+APPInit();
 
 
 // **************************************页面加载运行 end  *************************************
@@ -27,15 +31,126 @@ indexInit();
 
 
 
+//所有页面footer处理（本地化存储获取）
+function localFooterInit() {
+    $("#footer_address").text(localStorage.footer_address);
+    $("#footer_phone").text(localStorage.footer_phone);
+    $("#footer_mail").text(localStorage.footer_mail);
+
+}
+
+//logo添加点击返回首页
+function toHome() {
+
+    //logo点击返回首页
+    $("#header_logo").click(function(){
+
+        window.location.href = "index.html"
+
+    });
+}
+
+//处理轮播
+function coversMove() {
+
+    $.ajax(  
+    {  
+        type:'get',  
+        url : INDEX_BANNERS,  
+        dataType : 'text json',  
+        jsonp:"jsoncallback",  
+        success  : function(response) {  
+
+            var coversHtml = "";
+
+            for(var i = 0; i < 4; i ++) {
+
+                var file = response.rows[i];
+
+                var item = "<img src='" + file.imgUrl + "'>";
+
+                coversHtml = coversHtml + item;
+
+            }
+
+            $("#banner_images").html(coversHtml);
+
+                $('#banner_images').slick({
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: true,
+                    autoplaySpeed: 3000,
+                });
 
 
+        },  
+        error : function() {
 
-//首页 获取处理数据
-function indexInit() {
+            console.log('获取首页数据失败');
+
+        }  
+    });
+}
+
+// 处理产品详情
+function productDetial(id) {
+
+    var urlTemp = PRODUCT_DETAIL + id;
+
+    $.ajax(  
+    {  
+        type:'get',  
+        url : urlTemp,  
+        dataType : 'text json',  
+        jsonp:"jsoncallback",  
+        success  : function(response) {  
+
+            console.log(response);
+
+            $("#product_title").text(response.title);
+
+            $("#product_date").text(response.createTimeStr);
+
+            $("#product_content").html(response.content);
+
+        },  
+        error : function() {
+
+            console.log('获取产品详情失败');
+
+        }  
+    });
+
+}
+
+//全局入口
+function APPInit() {
+
+    //处理轮播
+    coversMove();
+
+    //导航栏添加点击事件
+    makeNavClickable();
+
+    //二级子菜单添加点击事件
+    makeMenuClickable();
+
+    //处理页脚本地数据
+    localFooterInit();
+
+    //logo添加点击返回首页
+    toHome();
+
 
     if (document.title == "首页") {
 
-        //接口获取数据
+        //关于津驰更多点击按钮
+        $("#index_more_btn").click(function(){
+            window.location.href = "about_Intro.html"
+        });
+
+        //获取新闻以外的数据
             $.ajax(  
             {  
                 type:'get',  
@@ -50,6 +165,11 @@ function indexInit() {
                     $("#footer_address").text(response.data.address);
                     $("#footer_phone").text(response.data.phone);
                     $("#footer_mail").text(response.data.mail);
+
+                    //存储footer数据到本地
+                    localStorage.footer_address = response.data.address
+                    localStorage.footer_phone   = response.data.phone
+                    localStorage.footer_mail    = response.data.mail
 
                     //处理关于津驰图片和文本
                     $("#about_img").attr('src',response.data.imgUrl);
@@ -66,6 +186,57 @@ function indexInit() {
                 }  
             }  
         );
+
+        //处理第一条新闻
+        $.ajax(  
+            {  
+                type:'get',  
+                url : COMPANY_NEWS,  
+                dataType : 'text json',  
+                jsonp:"jsoncallback",  
+                success  : function(response) {  
+
+                    console.log(response);
+
+                    var firstNews = response.rows[0];
+
+                    $("#topNews_title").text(firstNews.title);
+
+                    $("#topNews_content").text(firstNews.abstr);
+
+
+                },  
+                error : function() {  
+                    console.log('获取首页数据失败')  
+                }  
+            }  
+        );
+
+        //处理头四条新闻
+        $("#listNews_companySelected").click(function(){
+
+            //处理红条的位置
+            $("#listNews_segment").css("margin-left","20px");
+
+            //处理头四条新闻
+            homeTop4(0);
+
+
+        });
+
+        //初始化调用
+        homeTop4(0);
+
+        //处理头三条动态
+        $("#listNews_industrySelected").click(function(){
+
+            //设置红条的位置
+            $("#listNews_segment").css("margin-left","130px");
+
+            //处理头四条动态
+            homeTop4(1);
+
+        });
 
     } else if (document.title == "津驰介绍") {
 
@@ -270,6 +441,77 @@ function indexInit() {
             }  
         );
 
+    } else if (document.title == "津驰产品") {
+
+        $.ajax(  
+        {  
+            type:'get',  
+            url : PRODUCTS_LIST,  
+            dataType : 'text json',  
+            jsonp:"jsoncallback",  
+            success  : function(response) {
+
+                var rows = response.rows;  
+
+                //列表生成
+                var listHtml = "";
+
+                for(var i = 0; i < rows.length; i ++) {
+
+                    var product = rows[i];
+
+                    var item = "<div class='item'><div class='dota'></div><div class='text'>" + product.title +"</div></div>";
+
+                    listHtml = listHtml + item;
+
+                }
+
+                $("#paras_outline").html(listHtml);
+
+                //处理选中状态
+                var titles = $("#paras_outline .item");
+
+                titles.each(function(index, item){
+
+                    $(item).click(function(){
+
+                        //清除所有已选中的选项
+                        titles.each(function(indexTemp, itemTemp){
+                            $(itemTemp).css("background","white");
+                        });
+
+                        //添加选中项背景色
+                        $(item).css("background","rgb(204, 204, 204)");
+
+                        //获取选中的详情
+                        let id = rows[index].id;
+
+                        productDetial(id);
+
+                    });
+
+                    if (index == 0) {
+
+                         //添加选中项背景色
+                        $(item).css("background","rgb(204, 204, 204)");
+
+                        //获取选中的详情
+                        let id = rows[index].id;
+
+                        productDetial(id);
+
+                    }
+
+                });
+
+            },  
+            error : function() {
+
+                console.log('获取首页产品失败');
+
+            }  
+        });
+
     }  else {
 
 
@@ -340,5 +582,46 @@ function makeMenuClickable(){
             });
     
         });
+
+}
+
+//首页 处理头四条新闻或行业动态
+function homeTop4(type) {
+
+    var urlTemp = COMPANY_NEWS;
+
+    if (type == 1) {
+
+        urlTemp = INDUSTRY_NEWS;
+
+    }
+
+    $.ajax(  
+    {  
+        type:'get',  
+        url : urlTemp,  
+        dataType : 'text json',  
+                jsonp:"jsoncallback",  
+        success  : function(response) {  
+
+            var tempHTML = ""
+
+            for (var i = 0; i < 4; i ++) {
+
+                var row = response.rows[i];
+
+                tempHTML = tempHTML + "<div class='item'>" + row.title + "</div>"
+
+            }
+
+            $("#listNews_items").html(tempHTML);
+
+        },  
+        error : function() { 
+
+            console.log('获取首页数据失败');
+
+        }  
+    });
 
 }
