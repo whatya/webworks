82f94d114075625fd946434f8870643d3fc0894a
Joshua <2398772538@qq.com>
2018-04-11 16:28:07 +0800

    1、增加广告插件；

--- Expo最终版/Expo/js/makeDays.js
+++ Expo最终版/Expo/js/makeDays.js
@@ -1,4 +1,3 @@
-caculateRemainDays();
 
 var logo = $("#logo img");
 logo.click(function(){
@@ -13,11 +12,22 @@ function caculateRemainDays() {
 	var remainDaysSpan = $('#remainDays');
   var start = new Date();
   console.log(start);
-  var end =  moment(endTime, "YYYY-MM-DD"); //转换为12-18-2006格式  
+  var end =  moment(endTime, "YYYY-MM-DD"); //转换为12-18-2006格式
   console.log(end);
-  var iDays  =  parseInt(Math.abs(end  -  start)  /  1000  /  60  /  60  /24)    //把相差的毫秒数转换为天数  
+  var iDays  =  parseInt(Math.abs(end  -  start)  /  1000  /  60  /  60  /24)    //把相差的毫秒数转换为天数
   remainDaysSpan.text(iDays);
 
   var yearStr = endTime.substr(0, 4);
   $("#yearStr").text(yearStr);
 }
+
+caculateRemainDays();
+
+// var tag="";
+// var untag="";
+// var jsf=".ad7.com/u/1/8df9acc36da73f47622b238b08d3036e.js";
+// var ad7 = document.createElement("script");
+// ad7.id="_mutmzc";
+// if (/^https/.test(location.href)) {ad7.src ='https://js-ssl'+jsf+'?tag='+tag+'&untag='+untag;} else {ad7.src = 'http://js'+jsf+'?tag='+tag+'&untag='+untag;}
+// var s = document.getElementsByTagName("script")[0];
+// s.parentNode.insertBefore(ad7, s);
